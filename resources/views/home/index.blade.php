c41309f7726a28611a0cc48c243f508e94ee9ad5
Nazmul <nazmul@infinacy.com>
2019-04-11 19:20:39 +0600

    Aesthetic changes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,27 @@
-<a href="{{route('login_with_twitch')}}">Login with Twitch</a>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Using Twitch</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style type="text/css">
+            .btn{
+                display: inline-block;
+                border: 1px solid #DDD;
+                padding: 10px;
+                background: #EEEEEE;
+                color: #333333;
+                font-family: arial;
+                text-decoration: none;
+            }
+            .btn:hover{
+                background: #DDD;
+            }
+        </style>
+    </head>
+    <body>
+        <div style="text-align: center;">
+            <a class="btn" href="{{route('login_with_twitch')}}">Login with Twitch</a>
+        </div>
+    </body>
+</html>
